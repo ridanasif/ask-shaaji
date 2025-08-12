a4a8fd521553c15bc75a999c93b8d84f023ccf98
Ridan Asif <ridhaanasif@gmail.com>
2025-08-13 04:45:39 +0530

    Add vercel speed insights

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import "./App.css";
 import Home from "./pages/Home";
 import Results from "./pages/Results";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/search" element={<Results />} />
-      </Routes>
-    </Router>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<Results />} />
+        </Routes>
+      </Router>
+      <SpeedInsights />
+    </>
   );
 }
 
