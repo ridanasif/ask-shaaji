c5a254bd76a8b2b8347e9150c1d4d2869200d05c
Sufiyan Abdul Salam <sufiyansalam2006@gmail.com>
2025-09-05 21:06:01 +0530

    Adde new comic

--- src/data/comicsData.jsx
+++ src/data/comicsData.jsx
@@ -6,5 +6,13 @@ export const comics = [
     image: "/VIRAL-SHAAJI-COVER.png",
     comic: "/VIRAL-SHAAJI-COMIC.png",
   },
+
+  {
+    name: "It's A Match!",
+    slug: "its-a-match",
+    date: "05-09-2025",
+    image: "/ITS-A-MATCH-COVER.png",
+    comic: "/ITS-A-MATCH-COMIC.png",
+  },
   // Add all other comics here
 ];
